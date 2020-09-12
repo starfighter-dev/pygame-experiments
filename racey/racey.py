8320e6e558edd9b5cc0f827446d49d358e76d8af
Martin Elliott <martin@chirohosting.com>
2020-09-12 12:08:26 +0100

    moving things

--- racey/racey.py
+++ racey/racey.py
@@ -1,22 +1,29 @@
+#https://www.youtube.com/watch?v=ujOTNg17LjI
+
 import pygame
 
 pygame.init()
 
-gameDisplay = pygame.display.set_mode((800,600))
+display_width = 800
+display_height = 600
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
 
 crashed = False
 
 while not crashed:
-
    for event in pygame.event.get():
       if event.type == pygame.QUIT:
          crashed = True
-
       print(event)
-
    pygame.display.update()
-
    clock.tick(60)
 
+pygame.quit()
+quit()
