17248b9affe86e51f201f8b2b04ec1691ce24bc8
Martin Elliott <martin@chirohosting.com>
2020-09-12 12:20:00 +0100

    adding a car.

--- racey/racey.py
+++ racey/racey.py
@@ -15,13 +15,33 @@ gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
 
+carImg = pygame.image.load('car.png')
+
+# my image is too big.. scale it
+carImg = pygame.transform.scale(carImg, (64, 84))
+
+def car(x,y):
+   gameDisplay.blit(carImg,(x,y))
+
+x = (display_width * 0.45)
+y = (display_height * 0.8)
+
 crashed = False
 
 while not crashed:
    for event in pygame.event.get():
       if event.type == pygame.QUIT:
          crashed = True
+
+
+
+
+
+      #debug
       print(event)
+
+   gameDisplay.fill(white)
+   car(x,y)
    pygame.display.update()
    clock.tick(60)
 
